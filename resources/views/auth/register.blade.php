4e59a38749c0502c94f0e09b6747973f57fb14a7
erro-remainder <erro-remainder@ashivudhi.fritz.box>
2020-07-08 22:21:19 +0200

    initial

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,149 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Register') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Profesion') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Hair Cut') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Religion') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Hair Color') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Facial Hair') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Time Taken') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Feedback') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Barber Id') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                                @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Record') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/regressionmodel.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.app')
+<?php
+
+class RegressionTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testCSVImportWorksCorrectly()
+    {
+        $reg = new \mnshankar\LinearRegression\Regression();
+        //dependent variable is vas1.. column number 1
+        //independent columns are indep1, indep2, indep3 and indep4.. column numbers 9,10,11 and 12
+        $reg->loadCSV('dataset.csv', [1], [9, 10, 11, 12]);
+        $this->assertEquals($reg->getX(), $this->getXForTesting());
+        $this->assertEquals($reg->getY(), $this->getYForTesting());
+    }
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidSetXException()
+    {
+        $reg = new \mnshankar\LinearRegression\Regression();
+        $reg->setX([]);
+    }
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testInvalidSetYException()
+    {
+        $reg = new \mnshankar\LinearRegression\Regression();
+        $reg->setY([]);
+    }
+    /**
+     *
+     */
+    public function testRegressionComputation()
+    {
+        $reg = new \mnshankar\LinearRegression\Regression();
+        $reg->setX($this->getXForTesting());
+        $reg->setY($this->getYForTesting());
+        $reg->compute();
+        $this->assertEquals(0.3956, $reg->getRSquare(), null, .01);
+        $this->assertEquals(1.800187032, $reg->getF(), null, .01);
+        $this->assertEquals(331.75, $reg->getSSTOScalar());
+        $this->assertEquals(200.5, $reg->getSSEScalar());
+        $this->assertEquals(131.25, $reg->getSSRScalar());
+        $this->assertEquals(0.628990651, $reg->getMultipleR(), null, .01);
+        $this->assertEquals(16, $reg->getObservations());
+        $stdErrors = $reg->getStdErrors();
+        $pValues = $reg->getPValues();
+        $tStat = $reg->getTStats();
+        $coefficients = $reg->getCoefficients();
+        //The values to test against is obtained from excel for this data.
+        //refer to attached excel workbook Regression_Verification.xlsx
+        $coefficientsToTest = [10, -1, 0.75, -2.5, 5];
+        $stdErrorsToTest = [6.492346893, 2.134670509, 2.134670509, 2.134670509, 2.134670509];
+        $tStatToTest = [1.54027506, -0.468456371, 0.351342278, -1.171140928, 2.342281855];
+        $pValuesToTest = [0.151751456, 0.648604269, 0.731968834, 0.26628656, 0.039014953];
+        $this->assertEquals($coefficientsToTest, $coefficients, null, .01);
+        $this->assertEquals($stdErrorsToTest, $stdErrors, null, .01);
+        $this->assertEquals($tStatToTest, $tStat, null, .01);
+        $this->assertEquals($pValuesToTest, $pValues, null, .01);
+    }
+    /**
+     * @return array
+     */
+    private function getXForTesting()
+    {
+        return [
+            [1, 2, 1, 2, 2],
+            [1, 2, 2, 2, 2],
+            [1, 2, 1, 2, 1],
+            [1, 2, 2, 2, 1],
+            [1, 2, 1, 1, 2],
+            [1, 2, 2, 1, 2],
+            [1, 2, 1, 1, 1],
+            [1, 2, 2, 1, 1],
+            [1, 1, 1, 2, 2],
+            [1, 1, 2, 2, 2],
+            [1, 1, 1, 2, 1],
+            [1, 1, 2, 2, 1],
+            [1, 1, 1, 1, 2],
+            [1, 1, 2, 1, 2],
+            [1, 1, 1, 1, 1],
+            [1, 1, 2, 1, 1],
+        ];
+    }
+    /**
+     * @return array
+     */
+    private function getYForTesting()
+    {
+        return [
+            [12],
+            [12],
+            [13],
+            [7],
+            [21],
+            [22],
+            [9],
+            [7],
+            [9],
+            [16],
+            [11],
+            [17],
+            [16],
+            [19],
+            [13],
+            [10],
+        ];
+    }
+}
